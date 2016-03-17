372a80fdc45da7af7b83125fa75575fd02f6827b
Itay Sabato <itaysabato@gmail.com>
2016-03-17 14:22:52 +0000

    Added assertions of convergence and existence of NE.

--- src/test/java/il/ac/huji/cs/itays04/games/impl/SimpleGameAnalyzerTest.java
+++ src/test/java/il/ac/huji/cs/itays04/games/impl/SimpleGameAnalyzerTest.java
@@ -10,6 +10,7 @@ import il.ac.huji.cs.itays04.voting.VotingGame;
 import il.ac.huji.cs.itays04.voting.VotingGameState;
 import il.ac.huji.cs.itays04.voting.quadratic.QuadraticFactory;
 import org.apache.commons.math3.fraction.BigFraction;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
@@ -21,11 +22,28 @@ public class SimpleGameAnalyzerTest {
     //todo: add asserts
 
     @Test
-    public void analyzeProposition5Example() {
+    public void analyzeTheorem5Example() {
         final List<Integer> voterPositions = Lists.newArrayList(14,7,12);
         final Set<Integer> candidatePositions = Sets.newHashSet(17,2,10);
 
-        analyzeAndReport(voterPositions, candidatePositions, "Proposition 5 example");
+        final GameAnalysis<VotingGameState<Integer>, BigFraction> analysis = analyzeAndReport(
+                voterPositions, candidatePositions, "Theorem 5 example");
+
+        assertConvergence(analysis, true);
+    }
+
+    public void assertConvergence(
+            GameAnalysis<VotingGameState<Integer>, BigFraction> analysis,
+            boolean shouldConverge) {
+
+        final boolean doesConverge = analysis.getNumberOfNonSingularSccs() == 0;
+
+        if (shouldConverge) {
+            Assert.assertTrue("Game has cycles and thus may not converge", doesConverge);
+        }
+        else {
+            Assert.assertFalse("Game has no cycles and thus is guaranteed to converge", doesConverge);
+        }
     }
 
     @Test
@@ -33,7 +51,11 @@ public class SimpleGameAnalyzerTest {
         final List<Integer> voterPositions = Lists.newArrayList(16,4,1,7);
         final Set<Integer> candidatePositions = Sets.newHashSet(5,1,19,8);
 
-        analyzeAndReport(voterPositions, candidatePositions, "Theorem 8 example");
+        final GameAnalysis<VotingGameState<Integer>, BigFraction> analysis = analyzeAndReport(
+                voterPositions, candidatePositions, "Theorem 8 example");
+
+        assertConvergence(analysis, false);
+        assertNash(analysis, true);
     }
 
     @Test
@@ -41,7 +63,11 @@ public class SimpleGameAnalyzerTest {
         final List<Integer> voterPositions = Lists.newArrayList(400, 47, 92, 92, 92, 0, 0, 0, 0);
         final Set<Integer> candidatePositions = Sets.newHashSet(0,92,400);
 
-        analyzeAndReport(voterPositions, candidatePositions, "Theorem 10 example");
+        final GameAnalysis<VotingGameState<Integer>, BigFraction> analysis = analyzeAndReport(
+                voterPositions, candidatePositions, "Theorem 10 example");
+
+        assertConvergence(analysis, false);
+        assertNash(analysis, false);
     }
 
     @Test
@@ -49,7 +75,11 @@ public class SimpleGameAnalyzerTest {
         final List<Integer> voterPositions = Lists.newArrayList(15, 55, 42, 66, 74);
         final Set<Integer> candidatePositions = Sets.newHashSet(15, 55, 42, 66, 74);
 
-        analyzeAndReport(voterPositions, candidatePositions, "Theorem 11 example");
+        final GameAnalysis<VotingGameState<Integer>, BigFraction> analysis = analyzeAndReport(
+                voterPositions, candidatePositions, "Theorem 11 example");
+
+        assertConvergence(analysis, false);
+        assertNash(analysis, true);
     }
 
     @Test
@@ -57,10 +87,25 @@ public class SimpleGameAnalyzerTest {
         final List<Integer> voterPositions = Lists.newArrayList(4, 32, 34, 48, 67);
         final Set<Integer> candidatePositions = Sets.newHashSet(14, 32, 42, 60, 93);
 
-        analyzeAndReport(voterPositions, candidatePositions, "Theorem 12 example");
+        final GameAnalysis<VotingGameState<Integer>, BigFraction> analysis = analyzeAndReport(
+                voterPositions, candidatePositions, "Theorem 12 example");
+
+        assertConvergence(analysis, false);
+        assertNash(analysis, false);
     }
 
-    public void analyzeAndReport(List<Integer> voterPositions, Set<Integer> candidatePositions, String gameDescription) {
+    private void assertNash(GameAnalysis<VotingGameState<Integer>, BigFraction> analysis, boolean shouldExist) {
+        final boolean exists = analysis.getNeCount() > 0;
+
+        if (shouldExist) {
+            Assert.assertTrue("Game has no NE", exists);
+        }
+        else {
+            Assert.assertFalse("Game has a NE", exists);
+        }
+    }
+
+    public GameAnalysis<VotingGameState<Integer>, BigFraction> analyzeAndReport(List<Integer> voterPositions, Set<Integer> candidatePositions, String gameDescription) {
         System.out.println("Analyzing " + gameDescription + " with voters: " + voterPositions);
         final QuadraticFactory quadraticFactory = StaticContext.getInstance().getQuadraticFactory();
 
@@ -76,5 +121,7 @@ public class SimpleGameAnalyzerTest {
         final GameAnalysis<VotingGameState<Integer>, BigFraction> gameAnalysis = gameAnalyzer.analyze(game, brg);
 
         StaticContext.getInstance().getGameAnalysisReporter().printReport(game, gameAnalysis, System.out);
+
+        return gameAnalysis;
     }
 }
